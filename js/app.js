fb1eded9647572773d2e1e4cb0e429bc436ca7d0
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-03-01 14:36:15 +0100

    Js esercizi

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// console.log('ciao')
+
+// Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+
+let number = 100; //NUmber
+
+console.log(number)
+
+// Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+
+const PI = 3.1416; //NUmber
+
+console.log(PI)
+
+// Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+
+// const PI = 3.1416; //Number  Da errore perchè a una costante non passare cambiare il valore
+
+
+// Dichiara una variabile chiamata radius e assegnale il valore 8.
+// Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+// Stampa il valore della variabile circle in console
+
+let radius = 8; //Number
+const circle = radius * 2 * PI; //Number
+
+console.log(circle)
+
+
+// Dichiara una variabile chiamata name e
+// assegnale il valore “Carlo”.
+// Successivamente, modifica il valore della variabile in “Marco”.
+// Stampa il nuovo valore in console.
+
+let name = "Carlo"; //String
+name = "Marco" //String
+
+console.log(name)
